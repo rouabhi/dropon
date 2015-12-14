154abe49c3ba209c951a9482d331aedf13609836
Samir ROUABHI <cloudestin@gmail.com>
2015-12-14 12:51:22 +0100

    v1.0 tested..

--- dropon.js
+++ dropon.js
@@ -4,14 +4,14 @@ $.fn.dropon = function(action, value) {
       if ($a.length) {
         $dropon.find(".dropdown-value").text( $a.text() );
         $dropon.data("value", $a.data("value"));
+        $dropon.trigger( "dropdown:change" );
       }
   }
 
-  if (typeof action == "undefined") { // initialize
+  if (typeof action === "undefined") { // initialize
     $(this).each(function(index){
       var $dropon = $(this);
-      var $input = $(this).find("input.form-control");
-      var $button = $(this).find(".input-group-btn > button");
+      var $input = $dropon.find("input.form-control");
       var timer = null;
 
       if (!$input.length) return;
@@ -21,7 +21,7 @@ $.fn.dropon = function(action, value) {
         $input.focus();
       });
 
-      $button.click( function(){clearTimeout(timer);});
+      $dropon.find(".input-group-btn > button").click( function(){clearTimeout(timer);} );
 
       $input.keyup( function(e){
               var value = $dropon.data("value");
@@ -36,34 +36,51 @@ $.fn.dropon = function(action, value) {
                       .parent("li").nextAll().filter(":not(.divider):first").find("a"));
                   e.preventDefault();
                   break;
+                case 13 : // cr
+                  $dropon.trigger("dropon:submit");
+                  break;
+                case 27 : // ESC
+                  $dropon.dropon("toggle");
+                  break;
               }
             })
-        .focusin( function(){clearTimeout(timer); $dropon.dropon("open");} )
-        .focusout( function(){timer = setTimeout( function(){$dropon.dropon("close");}, 5000);} );
-
+          .change( function(){$dropon.trigger("dropon:change");} )
+          .focusin( function(){clearTimeout(timer); $dropon.trigger("dropon:focus");} )
+          .focusout( function(){timer = setTimeout( function(){$dropon.dropon("close");}, 2000); $dropon.trigger("dropon:blur");} );
       setValue($dropon, $dropon.find("ul.dropdown-menu li a[data-selected]"));
     });
   }
   else if (action === "open") {
-    $(this).addClass("open");
+    $(this).find(".input-group-btn").addClass("open");
   }
   else if (action === "close") {
-    $(this).removeClass("open");
+    $(this).find(".input-group-btn").removeClass("open");
   }
   else if (action === "toggle") {
-    $(this).toggleClass("open");
+    $(this).find(".input-group-btn").toggleClass("open");
   }
-  else if ((action === "VAL")) { // The values of the edit box x the factor from dropbox
-    return ($(this).data("value") * parseFloat($(this).find("input").val())) || 0;
+  else if ((action === "val")) { // The values of the edit box x the factor from dropbox
+    return ( parseFloat($(this).data("value") || 1) * parseFloat($(this).find("input").val())) || 0;
   }
-  else if ((action === "selected")&&(typeof val==="undefined")) { // getValue
+  else if ((action === "selected")&&(typeof value === "undefined")) { // getValue
     return ($(this).data("value"));
   }
-  else if ((action === "val")&&(typeof val==="undefined")) { // getValue
+  else if (action === "selected") { // setValue
+    var $dropon = this;
+    $dropon.find(".input-group-btn .dropdown-menu li a[data-value]").each(
+        function(){
+          var $a=$(this);
+          if ( value == $a.data("value") ) {
+            setValue($dropon, $a);
+            return false;
+          }            
+        });
+  }
+  else if ((action === "text")&&(typeof value === "undefined")) { // getValue
     return $(this).find("input").val();
   }
-  else if (action == "val") {
-    /* TODO : setValue*/
+  else if (action == "text") { // setValue
+    $(this).find("input").val( value );
   }
   else if (action.match &&  action.match(/^(focus|select)$/g)) {
     $(this).find("input")[action]();
